57ba4817f0a7273c3efb39bc2af1fe1fafb2540a
Sanris7 <115407290+Sanris7@users.noreply.github.com>
2024-09-13 21:28:41 +0530

    Update second.sh

--- second.sh
+++ second.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Create the directory if it doesn't exist
-DIRECTORY="/var/tmp/jenkins/services_running"
+# Define the directory where the report will be saved
+DIRECTORY="/var/tmp/services"
 if [ ! -d "$DIRECTORY" ]; then
   mkdir -p "$DIRECTORY"
 fi
@@ -9,11 +9,12 @@ fi
 # Get the current date and time for the filename
 DATE_TIME=$(date +"%Y-%m-%d_%H-%M-%S")
 
-# Filename for the report
+# Define the filename for the report
 FILENAME="$DIRECTORY/services_running_$DATE_TIME.txt"
 
-# List running services and save to the file
-systemctl list-units --type=service --state=running > "$FILENAME"
+# List all running services
+# You can use `ps` or another method if `systemctl` is not preferred
+ps -eo pid,comm,etime --sort=-etime | grep -v '^ *PID' > "$FILENAME"
 
 # Notify the user
 echo "Service report saved to $FILENAME"
